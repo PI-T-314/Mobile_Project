26b811977b4a7203bdae6e140301f9c8376bc9c4
PI-T-314 <rmaitynaeem@gmail.com>
2023-11-18 18:53:15 +0200

    Character Movement

--- lib/home.dart
+++ lib/home.dart
@@ -9,37 +9,63 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double characterX = 0;
 
-  void moveForward(){
-
+  void moveRight() {
+    setState(() {
+      if (characterX + 0.1 > 1) {
+        //on the edge of the screen
+      } else {
+        characterX += 0.1;
+      }
+    });
   }
-  void moveBackward(){
 
+  void moveLeft() {
+    setState(() {
+      if (characterX - 0.1 < -1) {
+        //on the edge of the screen
+      } else {
+        characterX -= 0.1;
+      }
+    });
   }
-  void shoot(){
 
-  }
+  void shoot() {}
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Expanded(
-          flex: 3,
+            //blue area
+            flex: 3,
             child: Container(
-          color: Colors.blueAccent,
-        )
-        ),
-        Expanded(child: Container(
+                color: Colors.blueAccent,
+                child: Stack(
+                  children: [
+                    Container(
+                      alignment: Alignment(characterX, 1),
+                      child: Container(
+                        color: Colors.yellow,
+                        height: 50.0,
+                        width: 50.0,
+                      ),
+                    )
+                  ],
+                ))),
+        Expanded(
+            //grey area
+            child: Container(
           color: Colors.grey,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Button(icon: Icons.navigate_before, function: moveForward),
-                Button(icon: Icons.barcode_reader, function: shoot),
-                Button(icon: Icons.navigate_next, function: moveBackward),
-              ],
-            ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Button(icon: Icons.navigate_before, function: moveLeft),
+              Button(icon: Icons.barcode_reader, function: shoot),
+              Button(icon: Icons.navigate_next, function: moveRight),
+            ],
+          ),
         ))
       ],
     );
