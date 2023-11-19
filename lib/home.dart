3f87462cc580a1319e0216e0e5f1f54271af94d8
PI-T-314 <rmaitynaeem@gmail.com>
2023-11-19 20:56:28 +0200

    physics

--- lib/home.dart
+++ lib/home.dart
@@ -12,22 +12,22 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
- enum BallDirection{
-   BALLLEFT,
-   BALLRIGHT,
-  }
+enum BallDirection {
+  BALLLEFT,
+  BALLRIGHT,
+}
 
 class _HomeState extends State<Home> {
   static double characterX = 0;
   double lazerX = characterX;
   double lazerHeight = 15;
   bool isShooting = false;
-   
-  double ballX=0.0;
-  double ballY=0.0;
-  
-  var balldirection=BallDirection.BALLLEFT;
-  
+
+  double ballX = 0.9;
+  double ballY = 1;
+
+  var balldirection = BallDirection.BALLLEFT;
+
   void moveRight() {
     setState(() {
       if (characterX + 0.1 > 1) {
@@ -80,68 +80,87 @@ class _HomeState extends State<Home> {
     isShooting = false;
     lazerX = characterX;
   }
-  void  gameloop(){
-     Timer.periodic(const Duration(milliseconds: 35), (timer) {
+
+  void gameloop() {
+    double height = 0;
+    double time = 0;
+    double velocity = 60;
+    Timer.periodic(const Duration(milliseconds: 35), (timer) {
       setState(() {
-         if (ballX- 0.01 < -1) {
-           balldirection= BallDirection.BALLRIGHT;
-         }
+        height = -5 * time * time + velocity * time;
+        ballY = heightToY(height);
 
-         if (ballX + 0.01 > 1) {
-          balldirection=BallDirection.BALLLEFT;
-         }
-
-        if(balldirection==BallDirection.BALLLEFT){
-             ballX-=0.01;
-        }else{
-          ballX+=0.01;
+        if (ballY > 1) {
+          time = 0;
         }
+
+        if (ballX - 0.02 < -1) {
+          balldirection = BallDirection.BALLRIGHT;
+        }
+
+        if (ballX + 0.02 > 1) {
+          balldirection = BallDirection.BALLLEFT;
+        }
+
+        if (balldirection == BallDirection.BALLLEFT) {
+          ballX -= 0.02;
+        } else {
+          ballX += 0.02;
+        }
+
+        time += 0.1;
       });
-     });
-     
+    });
   }
+
+  double heightToY(double height) {
+    double totalHeight = MediaQuery.of(context).size.height * (3 / 4);
+    double Y = 1 - 2 * (height / totalHeight);
+    return Y;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector( 
+    return GestureDetector(
       onTap: gameloop,
-      child:Column(
-      children: [
-        Expanded(
-            //blue area
-            flex: 3,
-            child: Container(
-                color: Colors.blueAccent,
-                child: Stack(
-                  children: [
-                    Container(
-                        alignment: Alignment(lazerX, 1),
-                        child: Container(
-                          color: Colors.red,
-                          height: lazerHeight,
-                          width: 5.0,
-                        )),
-                        
-                        Ball(ballX: ballX,ballY: ballY,),
-                    Charecter(characterX: characterX),
-                  ],
-                ))),
-        Expanded(
-            //grey area
-            child: Container(
-          color: Colors.grey,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Button(icon: Icons.navigate_before, function: moveLeft),
-              Button(icon: Icons.barcode_reader, function: shoot),
-              Button(icon: Icons.navigate_next, function: moveRight),
-            ],
-          ),
-        ))
-      ],
-    ),
+      child: Column(
+        children: [
+          Expanded(
+              //blue area
+              flex: 3,
+              child: Container(
+                  color: Colors.blueAccent,
+                  child: Stack(
+                    children: [
+                      Container(
+                          alignment: Alignment(lazerX, 1),
+                          child: Container(
+                            color: Colors.red,
+                            height: lazerHeight,
+                            width: 5.0,
+                          )),
+                      Ball(
+                        ballX: ballX,
+                        ballY: ballY,
+                      ),
+                      Charecter(characterX: characterX),
+                    ],
+                  ))),
+          Expanded(
+              //grey area
+              child: Container(
+            color: Colors.grey,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Button(icon: Icons.navigate_before, function: moveLeft),
+                Button(icon: Icons.barcode_reader, function: shoot),
+                Button(icon: Icons.navigate_next, function: moveRight),
+              ],
+            ),
+          ))
+        ],
+      ),
     );
   }
 }
-
-
